b1c8873d1847a42009deb25711a198f52b7ea6af
CharlieCoyle <charliecoyle41198@hotmail.com>
2018-11-25 18:58:38 +0000

    added profile page.

--- SourceCode/temp.py
+++ SourceCode/temp.py
@@ -33,13 +33,21 @@ def  init_db():
 
 def  check_auth(username , password):
 	db = get_db()
-	info = db.cursor().execute("SELECT username,password FROM accounts")
-	info = info.fetchall()
-	pasword = password.encode("utf-8")
-	for item in (info):
-		if(username == item[0] and item[1].encode("utf-8") == bcrypt.hashpw(password.item[1].encode("utf-8"))):
+	data = db.cursor().execute("SELECT username,password FROM accounts")
+	data = data.fetchall()
+	if data is None:
+		print("wasnt saved bucko")
+	password = password.encode("utf-8")
+	for item in (data):
+		if(username == item[0] and item[1].encode("utf-8") == bcrypt.hashpw(password,item[1].encode("utf-8"))):
 			return True
 	return False
+def  update_account(conn, account):
+	sql =''' UPDATE accounts
+		SET favoraties = ?
+		WHERE username = ?'''
+	cur = conn.cursor()
+	cur.execute(sql, account)
 
 
 @app.route("/AdventureNation/Register", methods=["POST","GET"])
@@ -47,33 +55,48 @@ def  Registration():
 
         db = get_db()
         if request.method == "POST":
+		db = get_db()
                 username = request.form["username"]
                 password = request.form["password"]
                 password = password.encode("utf-8")
-                hashedpw = bcrypt.hashpw(password, bcrypt.gensalt)
-		favorite = None
+                hashedpw = bcrypt.hashpw(password, bcrypt.gensalt())
+		favorite = 0
                 if (hashedpw is not None and username is not None):
-                        db.cursor.execute("INSERT INTO accounts(username, password, favorite) values (?,?,?)",(username,hashedpw,favorite))
-                        db.comit()
+			print(hashedpw)
+			print(username)
+                        db.cursor().execute("INSERT INTO accounts(username, password, favorite) values (?,?,0)",(username,hashedpw))
+                        db.commit()
 			return redirect (url_for(".SignIn"))
         return render_template("SignUp.html")
-
 
 @app.route("/AdventureNation/SignIn", methods=["POST","GET"])
 def  SignIn():
 	db = get_db()
 	if request.method == "POST":
-		username = request.form['Username']
-		password = request.form['Password']
+		username = request.form["username"]
+		password = request.form["password"]
 		if check_auth(username,password):
 			session['Curent_User'] = username
 			return redirect (url_for(".HomepageSlected"))
 	return render_template("SignIn.html")
 
 #Home Page
-@app.route('/AdventureNation')
+@app.route('/AdventureNation', methods=["POST","GET"])
 def  HomepageSlected(name=None):
-        return  render_template('home.html')
+	if request.method == "POST":
+		db = get_db
+		favorite = request.form["FaveLocation"]
+		conn = create_connection(db)
+		with conn:
+			Curent_user = session['Curent_User']
+			if Curent_user is not null:
+				update_account(conn,(favorite,Curent_User))
+				return  render_template('home.html',Curent_User=Curent_User)
+	try:
+		Curent_User = session['Curent_User']
+		return  render_template('home.html',Curent_User=Curent_User)
+	except:
+		return  render_template('home.html')
 #Item Selected Page
 @app.route('/AdventureNation/Situation')
 @app.route('/AdventureNation/Situation/')
@@ -92,7 +115,12 @@ def  GameSlected(game=None,place=None):
 		#for i, e in enumerate(data):
        		#	if  place == next(iter(e)):
             	#		return render_template('situation.html', game=game, data=e, place=place)
-        		return render_template('situation.html',UrlHelper=UrlHelper ,game=game, data=data, place=place)
+			try:
+				Curent_User = session['Curent_User']
+				return render_template('situation.html',UrlHelper=UrlHelper ,game=game, data=data, place=place, Curent_User=Curent_User)
+			except:
+				Curent_User = None
+        			return render_template('situation.html',UrlHelper=UrlHelper ,game=game, data=data, place=place, Curent_User=Curent_User)
 		return render_template('situation.html', game=game, place=place)
 	except IOError:
 		page ='''
@@ -106,6 +134,26 @@ def  GameSlected(game=None,place=None):
 def  postJsonHandler(item=None):
     url = url_for ('static', filename = item + '.json')
     return "<a href=%s>file</a>" %  url
+
+@app.route('/AdventureNation/Profile')
+def  proflieload():
+	favorite = None
+	Curent_User = None
+	try:
+		Curent_User = session['Curent_User']
+	except:
+		favorite = None
+	favrite = "GiantsLand"
+	try:
+		db = db_get()
+		Curent_User = session['Curent_User']
+		for item in  db.cursor().execute("SELECT * FROM accounts WHERE username='"+ Curent_User +"'"):
+				favorite = item[2]
+				print("WE did a thing")
+		return render_template('Profile.html', Curent_User=Curent_User ,favorite=favorite)
+	except:
+		return render_template('Profile.html',Curent_User=Curent_User ,favorite=favorite)
+
 
 #error out page
 @app.errorhandler(404)
